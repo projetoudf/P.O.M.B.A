1b3bef003ef41ab763c39a06f57b55e9b6d747fd
willian <willianwt@gmail.com>
2019-05-22 10:24:15 -0300

    hotfix

--- curso/excluirdisciplina.php
+++ curso/excluirdisciplina.php
@@ -9,7 +9,7 @@
     
     $res = $conn->multi_query($sql) or die($conn->error);
     if ($res == true) {
-        print "<script>alert('Excluiu a falta com sucesso!');window.history.go(-2)</script>";
+        print "<script>alert('Excluiu a disciplina com sucesso!');window.history.go(-2)</script>";
     } else {
         print "<br><div class='alert alert-danger'>Não foi possível deletar.</div>; window.history.go(-1);";
     }

69880b5405a4ccd7fa58084523887ac7fc9790aa
Aluno 4Rlab02 <aluno.4rlab02@acadnet.aeudf.br>
2019-05-21 21:49:00 -0300

    background provisório

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,8 @@
-<h1>Bem vindo</h1>
+<style>
+    body{
+        background-image: url(POMBO1.JPG);
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+</style>
+<h1>Bem vindo e bons estudos</h1>

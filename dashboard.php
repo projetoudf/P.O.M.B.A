e6de024771be403db5f7d5a7e86d8fdfac93d038
Willian Taiguara Baliza Moura <willianwt@gmail.com>
2019-06-12 06:47:38 -0300

    Imagem removida

--- dashboard.php
+++ dashboard.php
@@ -1,8 +1,2 @@
-<style>
-    body{
-        background-image: url("POMBO1.jpg");
-        background-repeat: no-repeat;
-        background-position: center;
-    }
-</style>
+
 <h1>Bem vindo e bons estudos</h1>
